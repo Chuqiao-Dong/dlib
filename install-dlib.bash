ec0283ea69fdbd1f8896cb44794c6be8ef1f02ea
Chuqiao-Dong <chuqiao.dong@ubtrobot.com>
2021-06-04 16:14:40 -0700

    add bash file for installation

---
+++ install-dlib.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/davisking/dlib.git
+cd dlib
+git checkout nx-4.4.1
+
+mkdir build
+cd build
+cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1
+cmake --builld .
+sudo make install
+cd ..
+python3 setup.py install
